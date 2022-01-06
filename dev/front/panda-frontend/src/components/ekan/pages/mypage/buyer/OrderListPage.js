5e725cadcbfc76fb37c84afc7948a87094ec46ea
ekank <ekankr2@naver.com>
2022-01-07 08:15:43 +0900

    주문 내역 디자인 수정 && 더보기 기능 추가

--- dev/front/panda-frontend/src/components/ekan/pages/mypage/buyer/OrderListPage.js
+++ dev/front/panda-frontend/src/components/ekan/pages/mypage/buyer/OrderListPage.js
@@ -1,50 +1,95 @@
-import React, {useEffect} from 'react';
-import ProductCard from "../../../UI/cards/ProductCard";
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
+import Button from "../../../UI/Button";
+import axios from "axios";
 import {setError} from "../../../../../store/actions/pageActions";
-import {fetchDashBoard, fetchSituationList} from "../../../../../store/actions/mypageActions/buyerActions";
+import {fetchSituationList, fetchSituationWithPage} from "../../../../../store/actions/mypageActions/buyerActions";
+import RecentOrderCard from "../../../UI/cards/RecentOrderCard";
 
 const OrderListPage = () => {
     const {error} = useSelector((state) => state.page);
     const {situationList} = useSelector((state) => state.buyer);
+    const [pagedData, setPagedData] = useState(null)
+    const [currPage, setCurrPage] = useState(1)
+    const [loading, setLoading] = useState(false)
     const dispatch = useDispatch()
+
+    console.log('페이지데이타: ', pagedData)
 
 
     useEffect(() => {
         if (error) {
             dispatch(setError(''))
         }
-        dispatch(fetchDashBoard())
-        dispatch(fetchSituationList())
+        dispatch(fetchSituationWithPage(5, 0))
     }, [])
+
+    useEffect(() => {
+        if (currPage === 1) {
+            setPagedData(situationList?.pageList)
+        }
+
+    }, [situationList])
 
     function orderBtnText(status) {
         switch (status) {
             case '배송완료':
                 return '구매 확정'
             case '배송중':
-                return '배송 정보'
+                return '상세 보기'
             case '상품준비중':
-                return '취소요청'
+                return '상세 보기'
             case '구매확정':
                 return '재구매'
             default:
-                return '배송 정보'
+                return '상세 보기'
         }
+    }
+
+    function handleClick(e) {
+        e.preventDefault()
+
+        axios.get(`/api/recentsituation?size=${5}&page=${currPage}`)
+            .then((result) => {
+                console.log(result.data)
+                if (pagedData && result) {
+                    setPagedData([...pagedData, ...result.data.pageList])
+                }
+            })
+            .catch(() => {
+                console.log('더보기 실패')
+            })
+
+        console.log('결과: ', pagedData)
+        console.log('길이: ',pagedData?.length)
+
+        setCurrPage(currPage + 1)
+        console.log(currPage)
     }
 
     return (
         <>
             <h3 className="page-header">주문 현황</h3>
-            {situationList && situationList.pageList.map((item, index) =>
-                <ProductCard key={index}
-                             title={item.productName}
-                             image="https://semantic-ui.com/images/wireframe/image.png"
-                             price={item.price}
-                             status={item.status}
-                             btnText={orderBtnText(item.status)}
+            {pagedData && pagedData.map((item, index) =>
+                <RecentOrderCard key={index}
+                                 title={item.productName}
+                                 price={item.price}
+                                 status={item.status}
+                                 btnText={orderBtnText(item.status)}
                 />
             )}
+            {
+                pagedData?.length === situationList?.totalElement ? null
+                    :
+                    <div className="mt-5" style={{textAlign: "center"}}>
+                        <Button onClick={handleClick}
+                                text="더보기"
+                                className={loading ? 'is-primary is-rounded is-loading is-medium'
+                                    : "is-primary is-rounded is-medium"}
+                                disabled={loading}/>
+                    </div>
+            }
+
         </>
     );
 };
