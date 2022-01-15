f2d90791eced5b9b9a48015e32acb3e2c13f9c2a
ekank <ekankr2@naver.com>
2022-01-15 23:40:08 +0900

    판다 대쉬보드 액션

--- dev/front/panda-frontend/src/store/actions/mypageActions/pandaActions.js
+++ dev/front/panda-frontend/src/store/actions/mypageActions/pandaActions.js
@@ -1,42 +1,61 @@
-import { FETCH_PANDA_SETTLEMENT_LIST } from "../../types";
+import {FETCH_PANDA_DASHBOARD, FETCH_PANDA_SETTLEMENT_LIST} from "../../types";
 import axios from "axios";
-import { setError } from "../pageActions";
+import {setError} from "../pageActions";
 
 export const fetchPandaDashBoard = () => {
-  return async (dispatch) => {};
+    return async dispatch => {
+        try {
+            let res = await axios.post('/api/pandadashboardmain', {
+                year: year
+            })
+            if (res.data) {
+                const data = res.data
+                console.log(data)
+                dispatch({
+                    type: FETCH_PANDA_DASHBOARD,
+                    payload: data
+                })
+            }
+        } catch (error) {
+            console.error(error)
+            onError()
+            dispatch(setError("판다 대시보드 이상"))
+        }
+    }
 };
 
 export const fetchPandaSettlementList = (data, onError) => {
-  return async (dispatch) => {
-    try {
-      console.log("페치데이타");
-      console.log(new Date(data.startDate).getDate());
+    return async (dispatch) => {
+        try {
+            console.log("페치데이타");
+            console.log(new Date(data.startDate).getDate());
 
-      const res = await axios.post("/api/pandadashboard", {
-        startYear: new Date(data.startDate).getFullYear(),
-        startMonth: new Date(data.startDate).getMonth(),
-        startDay: new Date(data.startDate).getDate(),
-        endYear: new Date(data.endDate).getFullYear(),
-        endMonth: new Date(data.endDate).getMonth(),
-        endDay: new Date(data.endDate).getDate(),
-        status: data.searchStatus,
-      });
-      if (res.data) {
-        const list = res.data;
-        console.log("판다 정산 목록: ", list);
-        dispatch({
-          type: FETCH_PANDA_SETTLEMENT_LIST,
-          payload: list,
-        });
-      }
-    } catch (error) {
-      console.log(error);
-      onError()
-      dispatch(setError("판다 정산 목록 이상"));
-    }
-  };
+            const res = await axios.post("/api/pandadashboard", {
+                startYear: new Date(data.startDate).getFullYear(),
+                startMonth: new Date(data.startDate).getMonth(),
+                startDay: new Date(data.startDate).getDate(),
+                endYear: new Date(data.endDate).getFullYear(),
+                endMonth: new Date(data.endDate).getMonth(),
+                endDay: new Date(data.endDate).getDate(),
+                status: data.searchStatus,
+            });
+            if (res.data) {
+                const list = res.data;
+                console.log("판다 정산 목록: ", list);
+                dispatch({
+                    type: FETCH_PANDA_SETTLEMENT_LIST,
+                    payload: list,
+                });
+            }
+        } catch (error) {
+            console.log(error);
+            onError()
+            dispatch(setError("판다 정산 목록 이상"));
+        }
+    };
 };
 
 export const fetchPandaSettlement = () => {
-  return async (dispatch) => {};
+    return async (dispatch) => {
+    };
 };
