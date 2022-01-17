e47c9fc4550194cace09e27938b981fd15e055f2
ekank <ekankr2@naver.com>
2022-01-17 15:26:15 +0900

    판다 비디오리스트 페이지

--- dev/front/panda-frontend/src/components/ekan/UI/cards/VideoCard.js
+++ dev/front/panda-frontend/src/components/ekan/UI/cards/VideoCard.js
@@ -1,47 +1,71 @@
-import React from "react";
+import React, {useEffect, useState} from 'react';
 import Button from "../Button";
-import "./videocard.css";
-const VideoCard = ({ title, image }) => {
+import "./videocard.css"
+import axios from "axios";
+
+function modifyBtnAction(e) {
+  e.preventDefault()
+  console.log('수정하기')
+}
+
+function deleteBtnAction(e) {
+  e.preventDefault()
+  console.log('삭제하기')
+}
+
+const VideoCard = ({link, panda}) => {
+  const [videoInfo, setVideoInfo] = useState(null)
+
+  useEffect(() => {
+    const fullUrl = `https://noembed.com/embed?url=${link}`
+    axios.get(fullUrl)
+        .then((result) => {
+          // console.log(result.data)
+          setVideoInfo(result.data)
+          return result.data
+        }).then((setVideoInfo)
+    )
+  }, [])
+
   return (
-    <>
-      <div className="card video-card">
-        <div className="card-image video-card-image">
-          <figure className="image is-4by3">
-            <img
-              src="https://img.youtube.com/vi/7HaJArMDKgI/mqdefault.jpg"
-              alt="Placeholder image"
-            />
-          </figure>
-        </div>
-        <div className="card-content">
-          <div className="media">
-            <div className="media-content">
-              <p className="video-title">{title}</p>
-            </div>
-          </div>
+      <>
+        {
+            videoInfo &&
+            <div className="card video-card">
+              <div className="card-image video-card-image">
+                <figure className="image is-4by3">
+                  <img src={videoInfo.thumbnail_url} alt="Placeholder image"/>
+                </figure>
+              </div>
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-content">
+                    <a className="video-title" target="_blank" href={link}>{videoInfo.title}</a>
+                  </div>
+                </div>
 
-          <div className="video-content">
-            <div>
-              <span className="video-sold">판매수 11,000회</span>
-              <span>
-                <i className="bx bx bx-won"></i>15만원
-              </span>
-            </div>
-          </div>
-        </div>
+                <div className="video-content">
+                  <div>
+                    <span className="video-sold">{panda}</span>
+                    <span>{videoInfo.author_name}</span>
+                  </div>
+                </div>
+              </div>
 
-        <footer className="card-footer has-background-white">
-          <div style={{ width: "100%" }}>
-            <span className="float-start">
-              <Button text="수정" className="is-info is-inverted" />
-            </span>
-            <span className="float-end">
-              <Button text="삭제" className="is-danger is-inverted" />
-            </span>
-          </div>
-        </footer>
-      </div>
-    </>
+              <footer className="card-footer has-background-white">
+                <div style={{width: "100%"}}>
+                        <span className="float-start">
+                            <Button onClick={modifyBtnAction} text="수정" className="is-info is-inverted"/>
+                        </span>
+                  <span className="float-end">
+                            <Button onClick={deleteBtnAction} text="삭제" className="is-danger is-inverted"/>
+                        </span>
+                </div>
+              </footer>
+            </div>
+        }
+
+      </>
   );
 };
 
