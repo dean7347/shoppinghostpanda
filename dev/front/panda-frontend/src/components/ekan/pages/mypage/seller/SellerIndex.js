9e32680197f0e0a34b029b1683eac1c6eda0667b
ekank <ekankr2@naver.com>
2022-01-19 01:07:17 +0900

    마이페이지 어스 리라우팅

--- dev/front/panda-frontend/src/components/ekan/pages/mypage/seller/SellerIndex.js
+++ dev/front/panda-frontend/src/components/ekan/pages/mypage/seller/SellerIndex.js
@@ -1,4 +1,4 @@
-import React, {FC} from 'react'
+import React, {FC, useState} from 'react'
 
 import '../buyer/buyerIndex.css'
 
@@ -6,19 +6,24 @@ import { Route } from 'react-router-dom'
 import {sellerSidebarItems} from "./sellerTypes";
 import MyPageRoutes from "../MyPageRoutes";
 import Sidebar from "../../../sections/Sidebar";
+import {getCookie} from "../../../../../store/Cookie";
+import Redirect from "react-router-dom/es/Redirect";
 
 const SellerIndex = () => {
+    const [seller] = useState(getCookie('seller'))
 
     return (
         <Route render={(props) => (
-            <div className={`layout theme-mode-light theme-color-blue`}>
-                <Sidebar sidebarItems={sellerSidebarItems} {...props}/>
-                <div className="layout__content">
-                    <div className="layout__content-main">
-                        <MyPageRoutes/>
+            seller === 'true' ?
+                <div className={`layout theme-mode-light theme-color-blue`}>
+                    <Sidebar sidebarItems={sellerSidebarItems} {...props}/>
+                    <div className="layout__content">
+                        <div className="layout__content-main">
+                            <MyPageRoutes/>
+                        </div>
                     </div>
-                </div>
-            </div>
+                </div> :
+                <Redirect to={"/shop"} />
         )}/>
     )
 }
